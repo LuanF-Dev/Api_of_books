b0f51afbaa174cbc9f684d82cb742d07313e24ea
Luan Ferreira <joaoluan821@gmail.com>
2023-01-02 20:10:52 -0300

    Atualização do arquivo index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { request } = require('express');
+// const { request } = require('express');
 const express = require('express');
 const server = express();
 // const books = require('./src/books.json');
